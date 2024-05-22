bed3d99296c3d77c8e8cace0787e5193f272fd68
vitatriutami <vitatriutami@gmail.com>
2024-05-22 08:36:05 +0700

    adding create and update todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,71 @@
-import { QuerySnapshot, collection, onSnapshot, query } from "firebase/firestore";
+import {
+  QuerySnapshot,
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  onSnapshot,
+  query,
+} from "firebase/firestore";
 import Form from "./components/Form";
 import Todo from "./components/Todo";
 import { useEffect, useState } from "react";
 import { db } from "./components/Firebase";
 
 const App = () => {
-  const [todo, setTodo] = useState(["code", "code more"])
-  
+  const [todo, setTodo] = useState([]);
+  const [input, setInput] = useState("");
+
   // CREATE TODO
+  const createTodo = async (e) => {
+    e.preventDefault();
+    if (input === "") {
+      alert("Please enter some task");
+      return;
+    }
+    await addDoc(collection(db, "todos"), {
+      text: input,
+      complete: false,
+    });
+    setInput("");
+  };
 
   // READ TODO
   useEffect(() => {
-    const q = query(collection(db, "todos"))
+    const q = query(collection(db, "todos"));
     const unsubscribe = onSnapshot(q, (QuerySnapshot) => {
-      const todosArr = []
+      const todosArr = [];
       QuerySnapshot.forEach((doc) => {
-        todosArr.push({...doc.data(), id: doc.id})
-      })
-      setTodo(todosArr)
-    })
-    return () => unsubscribe
-  }, [])
+        todosArr.push({ ...doc.data(), id: doc.id });
+      });
+      setTodo(todosArr);
+    });
+    return () => unsubscribe;
+  }, []);
 
   // UPDATE TODO
 
   // DELETE TODO
-  
-  
+  const deleteTodo = async (id: string) => {
+    await deleteDoc(doc(db, "todos", id));
+  };
+
   return (
     <div className="h-screen w-scree p-4 bg-teal-300">
       <div className="bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4">
-        <h1 className="text-3xl font-bold text-center text-gray-800">A Dev's Todo List</h1>
-        <Form />
+        <h1 className="text-3xl font-bold text-center text-gray-800">
+          A Dev's Todo List
+        </h1>
+        <Form createTodo={createTodo} input={input} setInput={setInput} />
         <ul>
           {todo.map((todos, index) => (
-            <Todo key={index} todos={todos}/>
+            <Todo key={index} todos={todos} deleteTodo={deleteTodo} />
           ))}
         </ul>
-        <p className="text-center">You have 2 things to complete</p>
+        <p className="text-center">
+          You have <span className="text-red-500">{todo.length}</span> things to
+          complete
+        </p>
       </div>
     </div>
   );
--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,11 +1,13 @@
 import CheckIcon from "@mui/icons-material/Check";
 import DeleteIcon from "@mui/icons-material/Delete";
+import { TodoType } from "../types";
 
 interface TodoProps {
-  todos: string;
+  todos: TodoType;
+  deleteTodo: (id: string) => void
 }
 
-const Todo = ({ todos }: TodoProps) => {
+const Todo = ({ todos, deleteTodo }: TodoProps) => {
   return (
     <li className="p-2 bg-teal-300 my-2 rounded-md uppercase shadow-sm">
       <div className="flex items-center justify-between">
@@ -14,7 +16,7 @@ const Todo = ({ todos }: TodoProps) => {
           <button>
             <CheckIcon />
           </button>
-          <button>
+          <button onClick={() => deleteTodo(todos.id)}>
             <DeleteIcon />
           </button>
         </div>
